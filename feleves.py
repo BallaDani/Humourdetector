5a91e2ee36fcc4a9f1520c451d2ae9ad83fa083a
BallaDani <balladanivadonjaro@gmail.com>
2023-12-12 18:29:40 +0100

    s

--- feleves.py
+++ feleves.py
@@ -92,11 +92,7 @@ def predict(text):
 
 
 
-def run(imp):
-
-
-    resp=' '
-    st.write(resp)
+def rund(imp):
 
 
     pred = predict(imp)
@@ -105,13 +101,13 @@ def run(imp):
         st.write('You are funny!')
     
     else:
-        st.write('You are Cringe')
+        st.write('You are Cringe!')
 
 st.write("Enter a funny joke")
 
 imp=st.text_input(label='Joke',value=" ", max_chars=300, type="default", help=None, on_change=None, args=None, kwargs=None, placeholder="Write your joke here", disabled=False, label_visibility="visible")
 
-st.button(label="Confirm", key=None, help=None, on_click=run(imp), args=None, kwargs=None,  type="secondary", disabled=False, use_container_width=False)
+st.button(label="Confirm", key=None, help=None, on_click=rund(imp), args=None, kwargs=None,  type="secondary", disabled=False, use_container_width=False)
 
 
 
