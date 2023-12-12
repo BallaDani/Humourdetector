0fbccfc0093e53df2caf21214fe656fd59a653fd
BallaDani <balladanivadonjaro@gmail.com>
2023-12-12 20:50:12 +0100

    n

--- feleves.py
+++ feleves.py
@@ -91,12 +91,13 @@ st.write("Enter a funny joke")
 imp=st.text_input(label='Joke',value=" ", max_chars=300, type="default", help=None,  args=None, kwargs=None, placeholder="Write your joke here", disabled=False, label_visibility="hidden")
 
 st.button(label="Test my Joke", on_click=pred(imp), args=None, kwargs=None,  type="secondary", disabled=False, use_container_width=False)
-ctr=1
+
 
 
 text = "what's the difference between donald trump's hair and a wet racoon"
 pred = predict(text)
 print("Text:",text)
+ctr=1
 print('Humor detected: ',pred)
 
    
