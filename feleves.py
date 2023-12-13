8890d4cec64f2483198c5fba4fb7e8dc9a5ab84c
BallaDani <balladanivadonjaro@gmail.com>
2023-12-13 10:48:27 +0100

    sds

--- feleves.py
+++ feleves.py
@@ -90,7 +90,11 @@ st.write("Enter a funny joke")
 
 imp=st.text_input(label='Joke',value=ctr, max_chars=300, type="default", help=None,  args=None, kwargs=None, placeholder="Write your joke here", disabled=False, label_visibility="hidden")
 
-st.button(label="Test my Joke", on_click=pred(imp), args=None, kwargs=None,  type="secondary", disabled=False, use_container_width=False)
+if imp:
+    pred(imp)
+
+#st.button(label="Test my Joke", on_click=pred(imp), args=None, kwargs=None,  type="secondary", disabled=False, use_container_width=False)
+
 
 
 
