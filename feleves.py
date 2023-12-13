60142dd866ca912f3a4b81ef21ccc87a02ddbb22
BallaDani <balladanivadonjaro@gmail.com>
2023-12-13 17:12:38 +0100

    h

--- feleves.py
+++ feleves.py
@@ -103,6 +103,9 @@ imp=st.text_input(label='Joke',value=defa, max_chars=300, type="default", help=N
 
 but=st.button(label="Test my Joke", args=None, kwargs=None,  type="secondary", disabled=False, use_container_width=False)
 box=st.selectbox("I don't want to enter my own joke: ",(" ","what's the difference between donald trump's hair and a wet racoon", "All pants are breakaway pants if you're angry enough","5 reasons the 2016 election feels so personal","Pasco police shot mexican migrant from behind, new autopsy shows"))
+if box:
+    defa=box
+
 if but:
 
     if imp!='':
